5ebf956628b52b7080dd4303037f3a04cf73a3a8
BenWurth <ben.wurth@gmail.com>
2013-09-04 15:20:06 -0700

    wordcountToWordcloud.r now outputs a wordcloud.

--- Wordcount/wordcountToWordcloud.r
+++ Wordcount/wordcountToWordcloud.r
@@ -1,5 +1,11 @@
 # This script takes a .csv file with two columns (words and wordcount) and turns them into a wordcloud.
 
+# Require wordcloud (otherwise we can't make a wordcloud!)
+require(wordcloud)
+
+# Flag for csv headers.
+csvHeaders <- FALSE
+
 # Get the csv file
 wordcountFile <- file.choose()
 
@@ -13,4 +19,13 @@ while(maxWords < 1 ){
   if(is.na(maxWords)){break}  # breaks when hit enter
 } 
 
-print(maxWords)
+# Puts the csv file into a data frame.
+if (!csvHeaders) # If there aren't any headers:
+{									# "Words" and "Count" are the default header names.
+	words.csv <- read.csv(wordcountFile, col.names = c("Words", "Count"), header=FALSE)
+} else {									# Headers are turned on
+	words.csv <- read.csv(wordcountFile, headers=TRUE)
+}
+
+# And now we get our wordcloud!
+wordcloud(words.csv[,1], words.csv[,2], max.words=maxWords)
